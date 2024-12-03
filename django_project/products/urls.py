1a780242536a08bb880b4b3eef5a976dffd650b7
Sean Li <cli049@uottawa.ca>
2024-12-03 11:35:54 -0500

    updated views.py and urls.py

---
+++ django_project/products/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import home, success, cancel
+
+urlpatterns = [
+    path("", home, name="home"),
+    path("success/", success, name="success"),
+    path("cancel/", cancel, name="cancel"),
+]
--- django_project/products/views.py
+++ django_project/products/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def home(request):
+    return render(request, "products/home.html")
+
+
+def success(request):
+    return render(request, "products/success.html")
+
+
+def cancel(request):
+    return render(request, "products/cancel.html")
