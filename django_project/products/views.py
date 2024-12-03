c3201172fa6bb3d4d5515a6410546466763bce1e
Sean Li <cli049@uottawa.ca>
2024-12-03 12:03:40 -0500

    updated views.py of app products to invoke strip api

--- django_project/products/views.py
+++ django_project/products/views.py
@@ -1,7 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.conf import settings
+from django.urls import reverse
+
+import stripe
 
 
 def home(request):
+    stripe.api_key = settings.STRIPE_SECRET_KEY
+    if request.method == "POST":
+        checkout_session = stripe.checkout.Session.create(
+            line_items=[
+                {
+                    "price": "price_1QRzOuC6sG47z0d5mrWTgKkG",  # Price API ID
+                    "quantity": 1,
+                }
+            ],
+            mode="payment",
+            success_url=request.build_absolute_uri(reverse("success")),
+            cancel_url=request.build_absolute_uri(reverse("cancel")),
+        )
+        return redirect(checkout_session.url, code=303)
     return render(request, "products/home.html")
 
 
